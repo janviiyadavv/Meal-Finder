582f71c1cb70684f71ab263b20a4ddb58b6c5bea
Janvi Yadav <112768387+janviiyadavv@users.noreply.github.com>
2023-07-23 18:05:15 +0530

    script.js

--- script.js
+++ script.js
@@ -121,17 +121,9 @@ function addMealToDOM(meal) {
 submit.addEventListener("submit", searchMeal);
 random.addEventListener('click',randomMeal);
 mealsEl.addEventListener("click", (e) => {
-  const mealInfo = e.path.find((item) => {
-    if (item.classList) {
-      return item.classList.contains("meal-info");
-    } else {
-      return false;
-    }
-  });
+  const mealInfo = e.target.closest(".meal-info");
   if (mealInfo) {
-    const mealID = mealInfo.getAttribute(
-      "data-mealid"
-    );
+    const mealID = mealInfo.getAttribute("data-mealid");
     getMealById(mealID);
   }
 });
